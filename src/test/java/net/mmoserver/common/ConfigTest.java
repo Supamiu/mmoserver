7e0c93db7d1e7fd928562ea43258b2223255b11b
Supamiu <contact@flavien-normand.fr>
2017-01-11 18:53:21 +0100

    Added Config and Log tests.

---
+++ src/test/java/net/mmoserver/common/ConfigTest.java
@@ -0,0 +1,17 @@
+package net.mmoserver.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Miu on 11/01/2017.
+ */
+public class ConfigTest {
+
+    @Test
+    public void simpleConstructorTest() {
+        Config config = new Config();
+        assertNotNull(config);
+    }
+}
